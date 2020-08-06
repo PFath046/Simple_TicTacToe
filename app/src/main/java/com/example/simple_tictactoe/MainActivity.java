7758d31157f3363b7391736ceec58c4994ee6429
PFath046 <67249045+PFath046@users.noreply.github.com>
2020-08-05 21:06:18 -0400

    3rd commit. Possibility of draw added. reset button added. The player can now play several rounds and have their scores recorded. Buttons now change colour based on which player clicks on them.

--- app/src/main/java/com/example/simple_tictactoe/MainActivity.java
+++ app/src/main/java/com/example/simple_tictactoe/MainActivity.java
@@ -1,44 +1,85 @@
 package com.example.simple_tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ * The class <b>MainActivity</b> is the
+ * class that implements and plays the Tic Tac Toe Game.
+ * It contains the grid and tracks its progress.
+ * It automatically maintain the current state of
+ * the game as players are making moves.
+ *
+ * @author Parsa Fathazam
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //Variable declaration
 
-    protected int moveCount = 0;
-    protected State state = State.PLAYING;
-    protected Button[][] grid = new Button[3][3];
-    protected int xScore = 0;
-    protected int oScore = 0;
+    private final int size = 4; //We set the size of the board according to the number of buttons.
+    private int moveCount = 0;
+    private State state = State.PLAYING;
+    private Button[][] grid = new Button[size][size];
+    private int oScore = 0;
+    private int xScore = 0;
+    private TextView oScoreView;
+    private TextView xScoreView;
 
-    public enum State{
+    private enum State{
         XWON,
         OWON,
         PLAYING,
-        DRAW;
+        DRAW
     }
 
-    public enum Value{
+    private enum Value{
         X,
         O,
         BLANK,
     }
 
-    private TextView tv;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = findViewById(R.id.textView);
+        xScoreView = findViewById(R.id.xScoreView);
+        xScoreView.setTextColor(Color.parseColor("#E91E63"));
+        xScoreView.setText("Player X: " + xScore);
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        oScoreView = findViewById(R.id.oScoreView);
+        oScoreView.setTextColor(Color.parseColor("#FF00BCD4"));
+        oScoreView.setText("Player O: " + oScore);
+
+        Button nextRound = findViewById(R.id.nextRound);
+        nextRound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (state != State.PLAYING){
+                    nextRound();
+                }
+            }
+        });
+
+        Button resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    reset();
+                }
+            });
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 String buttonID = "button" + i + j;
                 int ID = getResources().getIdentifier(buttonID, "id", getPackageName());
                 grid[i][j] = findViewById(ID);
@@ -63,7 +104,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     /**
-     * returns the value of button b.
+     * returns the value that button b contains.
      * @param b
      * button b
      * @return
@@ -74,13 +115,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return Value.X;
         } else if (b.getText().equals("O")){
             return Value.O;
-        }else {
+        } else {
             return Value.BLANK;
         }
     }
 
     /**
-     *returns the row of button b
+     *returns the row that contains button b
      * @param b
      * button b
      * @return the row which b is placed at
@@ -91,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     *returns the column of button b
+     *returns the column that contains button b
      * @param b
      * button b
      * @return the column which b is placed at
@@ -102,10 +143,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     * checks whether the player placed in button b has won the game.
+     * checks whether the player placed on button b has won the game.
      * @param b
      * button b
      */
+    @SuppressLint("SetTextI18n")
     protected void checkForWin(Button b){
         Value player = getValue(b);
         int countRow = 0;
@@ -116,14 +158,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int col = getCol(b);
 
         //Checks row
-        for (int i = 0; i < 3 ; i++){
+        for (int i = 0; i < size ; i++){
             if (getValue(grid[row][i]) == player){
                 countRow ++;
             }
         }
 
         //Checks column
-        for (int i = 0; i < 3 ; i++){
+        for (int i = 0; i < size ; i++){
             if (getValue(grid[i][col]) == player){
                 countCol ++;
             }
@@ -131,51 +173,124 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //Checks diagonal
         if (row == col) {
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < size; i++) {
                 if (getValue(grid[i][i]) == player) {
                     countDiag++;
                 }
             }
         }
 
-
         //Checks anti-diagonal
-        if (row + col == 3-1){
-            for (int i = 0; i < 3; i++) {
-                if (getValue(grid[i][2-i]) == player) {
+        if (row + col == size-1){
+            for (int i = 0; i < size; i++) {
+                if (getValue(grid[i][size-1-i]) == player) {
                     countAnti++;
                 }
             }
         }
 
-
-        if (countRow == 3 || countCol == 3 || countDiag == 3 || countAnti == 3){
+        //Sets game state based on the winner.
+        if (countRow == size || countCol == size || countDiag == size || countAnti == size){
             if (player == Value.X){
                 state = State.XWON;
+                xScore++;
+                xScoreView.setText("Player X: " + xScore);
             }
             else if (player == Value.O) {
                 state = State.OWON;
+                oScore++;
+                oScoreView.setText("Player O: " + oScore);
             }
+        }
+
+        else if (moveCount == size*size) {
+            state = State.DRAW;
+        }
+
+    }
+
+    /**
+     * Moves a player to button b depending on whose turn it is.
+     * @param b
+     * button b
+     */
+    protected void move(Button b) {
+            if (whosTurn() == Value.X) {
+                b.setText("X");
+            } else if (whosTurn() == Value.O) {
+                b.setText("O");
+            }
+            updateColor(b);
+            moveCount++;
+        }
+
+
+    /**
+     * Clears the grid for another round of the game.
+     *
+     */
+    protected void nextRound(){
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                Button b = grid[i][j];
+                b.setText("");
+                updateColor(b);
+            }
+        }
+        moveCount = 0;
+        state = State.PLAYING;
+    }
+
+    /**
+     * resets the entire game.
+     *
+     */
+    protected void reset(){
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                Button b = grid[i][j];
+                b.setText("");
+                updateColor(b);
+            }
+        }
+        oScore = 0;
+        xScore = 0;
+        oScoreView.setText("Player O: " + oScore);;
+        xScoreView.setText("Player X: " + xScore);;
+        moveCount = 0;
+        state = State.PLAYING;
+    }
+
+    /**
+     * Updates color of the button b depending on what value it contains.
+     * @param b
+     * button b
+     */
+    @SuppressLint("NewApi")
+    protected void updateColor(Button b){
+        Value player = getValue(b);
+        if (player == Value.X){
+            b.setBackgroundTintList(null);
+            b.setBackgroundColor(Color.parseColor("#E91E63"));
+        }
+        else if (player == Value.O){
+            b.setBackgroundTintList(null);
+            b.setBackgroundColor(Color.parseColor("#FF00BCD4"));
+        }
+        else {
+            b.setBackgroundResource(android.R.drawable.btn_default);
+            b.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFEB64")));
         }
     }
 
 
     public void onClick(View v) {
         Button b = (Button) v;
-        if (getValue(b) == Value.BLANK && state == State.PLAYING){
-            if (whosTurn() == Value.X){
-                b.setText("X");
-            }
-            else if (whosTurn() == Value.O){
-                b.setText("O");
-            }
-            moveCount ++;
-
+        if (getValue(b) == Value.BLANK && state == State.PLAYING) {
+            move(b);
             checkForWin(b);
-
         }
     }
-
 
 
 }
